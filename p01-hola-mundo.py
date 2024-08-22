4d696355d306a94219eaa67274499a870d0525ae
jortiz-v <ja.ov@outlook.com>
2024-08-22 13:05:26 -0600

    Actividad 1

---
+++ p01-hola-mundo.py
@@ -0,0 +1,16 @@
+# P01-hola-mundo - Recibe datos del usuario y los imprime en pantalla 
+
+nombre = input("Dame tu nombre: ")
+edad = int(input("Edad: "))
+peso =float(input("Peso: "))
+
+print(nombre)
+print(edad)
+print(peso)
+
+print(f"{nombre} Bienvenido a Python, tu edad es {edad} tu peso es {peso}")
+print(type(nombre))
+print(type(edad))
+print(type(peso))
+
+
---
+++ p03-area-triangulo.py
@@ -0,0 +1,9 @@
+# p03-area-triangulo - Calcula el area de un triangulo
+
+print("Calculando el area de un traingulo")
+print("Dame la base y la altura separados por un enter")
+base,altura = int(input()), int(input())
+
+area = base * altura /2
+
+print(f"Para un triangulo de base {base} y altura {altura} el area es {area:.2f}")
